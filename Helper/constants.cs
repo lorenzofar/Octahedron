2ba1b1733602a0517de2f530d0a78567c1a65f86
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-02-09 18:21:14 +0100

    Added dateTime formatters

--- Helper/constants.cs
+++ Helper/constants.cs
@@ -1,11 +1,16 @@
 ﻿using Octokit;
 using Windows.ApplicationModel.Resources;
+using Windows.Globalization.DateTimeFormatting;
 
 namespace Helper
 {
     public class constants
     {
         public static ResourceLoader r_loader = new ResourceLoader();
+        public static DateTimeFormatter shortDateFormatter = new DateTimeFormatter("shortdate");
+        public static DateTimeFormatter longDateFormatter = new DateTimeFormatter("longdate");
+        public static DateTimeFormatter shortTimeFormatter = new DateTimeFormatter("shorttime");
+        public static DateTimeFormatter longTimeFormatter = new DateTimeFormatter("longtime");
         public static GitHubClient g_client { get; set; }
     }
 }
