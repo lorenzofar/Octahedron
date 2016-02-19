3706965dfe6ece3caae9af2480f444920d8f15bd
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-02-19 22:51:15 +0100

    Added notifications list in main page. Added swipeable list view. Notifications are marked as read when swiped.

--- Github/ViewModels/MainViewModel.cs
+++ Github/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml.Navigation;
 using Helper;
 using System.Linq;
+using GalaSoft.MvvmLight.Command;
+using Universal.UI.Xaml.Controls;
 
 namespace Github.ViewModels
 {
@@ -26,6 +28,49 @@ namespace Github.ViewModels
             }
         }
 
+        private RelayCommand _RefreshFeed;
+        public RelayCommand RefreshFeed
+        {
+            get
+            {
+                if(_RefreshFeed == null)
+                {
+                    _RefreshFeed = new RelayCommand(LoadNotifications);
+                }
+                return _RefreshFeed;
+            }
+        }
+
+        private RelayCommand<object> _MarkNotificationRead;
+        public RelayCommand<object> MarkNotificationRead
+        {
+            get
+            {
+                if(_MarkNotificationRead == null)
+                {
+                    _MarkNotificationRead = new RelayCommand<object>( async(e) =>
+                    {
+                        if (e != null)
+                        {
+                            var swipeArgs = e as ItemSwipeEventArgs;
+                            Octokit.Notification readNotification = swipeArgs.SwipedItem as Octokit.Notification;
+                            try
+                            {
+                                await constants.g_client.Notification.MarkAsRead(int.Parse(readNotification.Id));
+                                LoadNotifications();
+                            }
+                            catch
+                            {
+                                await communications.ShowDialog("login_error", "error");
+                            }
+
+                        }
+                    });
+                }
+                return _MarkNotificationRead;
+            }
+        }
+
         public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
         {
             LoadNotifications();
@@ -34,7 +79,16 @@ namespace Github.ViewModels
 
         private async void LoadNotifications()
         {
-            notifications = (await constants.g_client.Notification.GetAllForCurrent()).Where(x => x.Unread == true).ToList();
+            try
+            {
+                notifications = (await constants.g_client.Notification.GetAllForCurrent()).Where(x => x.Unread == true).ToList();
+            }
+            catch
+            {
+                await communications.ShowDialog("login_error", "error");
+            }
         }
+
+        
     }
 }
