89bee6310021e701f99ce9362eb2e0141a71e736
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-02-28 14:57:50 +0100

    Added converter for notifications icons

---
+++ Github/Converters/NotificationIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace Github.Converters
+{
+    class NotificationIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var n_type = value.ToString();
+            string glyph = "";
+            switch (n_type.ToLower())
+            {
+                case "issue":
+                    glyph = "\uE783";
+                    break;
+                case "pullrequest":
+                    glyph = "\uEC0A";
+                    break;
+                case "release":
+                    glyph = "\uE8EC";
+                    break;
+            }
+            return glyph;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
