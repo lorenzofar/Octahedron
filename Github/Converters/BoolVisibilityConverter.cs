030618f2dc9f8c9595e7c41782fdb06611e24e0c
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-02-28 13:59:21 +0100

    Changed variable name in BoolVisibilityConverter

--- Github/Converters/BoolVisibilityConverter.cs
+++ Github/Converters/BoolVisibilityConverter.cs
@@ -8,8 +8,8 @@ namespace Github.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            bool selecting = parameter == null ? bool.Parse(value.ToString()) : !bool.Parse(value.ToString());
-            return selecting == true ? Visibility.Visible : Visibility.Collapsed;
+            bool visible = parameter == null ? bool.Parse(value.ToString()) : !bool.Parse(value.ToString());
+            return visible == true ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
