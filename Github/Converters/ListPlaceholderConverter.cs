890a8e5e1eaaf410425aee919bf82551131f8067
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-02-28 14:18:19 +0100

    Added placeholder when there are no notifications

---
+++ Github/Converters/ListPlaceholderConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Github.Converters
+{
+    class ListPlaceholderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null)
+            {
+                var cvs = value as List<Octokit.Notification>;
+                return cvs.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
