fc60f55993c360ee5a293b0c8ed9f253a76aa281
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-04-02 15:08:48 +0200

    Added converter to hide/show command bar in a pivot

---
+++ Github/Converters/PivotBarConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Github.Converters
+{
+    class PivotBarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int pivotIndex = int.Parse(value.ToString());
+            int barIndex = int.Parse(parameter.ToString());
+            return pivotIndex == barIndex ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
