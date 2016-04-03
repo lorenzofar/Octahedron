94450aa92a12958af8c73a3520616f173f25560f
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-04-03 10:30:52 +0200

    Added converter to make the issue icon green or red according to its status

---
+++ Github/Converters/IssueStateConverter.cs
@@ -0,0 +1,22 @@
+﻿using Octokit;
+using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Github.Converters
+{
+    class IssueStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ItemState state = (ItemState)value;
+            return state == ItemState.Open ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
