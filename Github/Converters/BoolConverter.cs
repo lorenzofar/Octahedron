7ae113c95d7c71ee7c4230b7281b991942637b2b
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-04-03 12:28:32 +0200

    Added bool converter

---
+++ Github/Converters/BoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Github.Converters
+{
+    class BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool state = bool.Parse(value.ToString());
+            return parameter == null ? state : !state;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
