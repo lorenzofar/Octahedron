64e25b5d701ec9e2824663a75af299e0e54ba494
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-04-18 17:56:27 +0200

    Improved readavility of repo's details

---
+++ Github/Converters/IssueDateConverter.cs
@@ -0,0 +1,22 @@
+﻿using Octokit;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Github.Converters
+{
+    class IssueDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Issue issue = value as Issue;
+            var user = issue.User.Login;
+            var openDate = issue.CreatedAt;
+            return String.Format(Helper.constants.r_loader.GetString("issueDate"), Helper.constants.shortDateFormatter.Format(openDate), user);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
