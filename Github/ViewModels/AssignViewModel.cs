5299572bf5f7be5bb7cd0613e9a53a7c9b38909f
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-05-14 17:03:45 +0200

    Improved assign issue dialog

--- Github/ViewModels/AssignViewModel.cs
+++ Github/ViewModels/AssignViewModel.cs
@@ -47,6 +47,10 @@ namespace Github.ViewModels
                 {
                 }
             }
+            else
+            {
+                assignSuggestions = null;
+            }
         }
     }
 }
