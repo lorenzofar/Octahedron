5fd57aa10e3bcc6950954cd1d67248d7ad85e14f
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-06-13 09:47:09 +0200

    Added blank search page

---
+++ Github/ViewModels/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace Github.ViewModels
+{
+    public class SearchViewModel : ViewModelBase
+    {
+
+    }
+}
