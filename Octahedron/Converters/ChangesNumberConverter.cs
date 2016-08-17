4328d313df096f4257939a6fe73d78ff280e5b02
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-08-17 14:27:14 +0200

    Added list of changed files in pull request details. Removed Application Insights config file.

---
+++ Octahedron/Converters/ChangesNumberConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Octahedron.Converters
+{
+    public class ChangesNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var changes = int.Parse(value.ToString());
+            if(parameter.ToString() == "add")
+            {
+                return $"+{changes}";
+            }
+            else
+            {
+                return $"-{changes}";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
