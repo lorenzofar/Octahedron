a6c206818a69d9332434e40dc1f8a41d154ca5c1
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-08-22 23:21:30 +0200

    Added list of commits in pull request details page. Added converters for commits' list.

---
+++ Octahedron/Converters/CommitInfoConverter.cs
@@ -0,0 +1,29 @@
+﻿using Helper;
+using Octokit;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Octahedron.Converters
+{
+    public class CommitInfoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(parameter != null && parameter.ToString() == "pull")
+            {
+                var commit = value as PullRequestCommit;
+                return $"{commit.Commit.Committer.Name} - {constants.shortDateFormatter.Format(commit.Commit.Committer.Date)}";
+            }
+            else
+            {
+                var commit = value as Commit;
+                return $"{commit.Committer.Name} - {constants.shortDateFormatter.Format(commit.Committer.Date)}";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
---
+++ Octahedron/Converters/ShaTrimmer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Octahedron.Converters
+{
+    public class ShaTrimmer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string sha = value.ToString();
+            return sha.Remove(7);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
