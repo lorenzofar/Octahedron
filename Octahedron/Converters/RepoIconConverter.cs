1f2dca3113b123899efd3c736512e99e8e938852
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-08-22 23:40:04 +0200

    Added new icon (lock) for private repositories

---
+++ Octahedron/Converters/RepoIconConverter.cs
@@ -0,0 +1,20 @@
+﻿using Octokit;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Octahedron.Converters
+{
+    public class RepoIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var repo = value as Repository;
+            return repo.Private ? "\uF06A" : "\uF001";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
