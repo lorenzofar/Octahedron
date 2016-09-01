75c16a7476e1b0d423736a2083aa93a2cc785920
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-09-02 00:05:57 +0200

    Fixed join date in profile page

--- Octahedron/Converters/JoinDateConverter.cs
+++ Octahedron/Converters/JoinDateConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Helper;
+using System;
 using Windows.UI.Xaml.Data;
 
 namespace Octahedron.Converters
@@ -8,7 +9,7 @@ namespace Octahedron.Converters
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             DateTime joinDateTime = ((DateTimeOffset)value).DateTime;
-            return String.Format(Helper.constants.r_loader.GetString("joinDate"), Helper.constants.shortDateFormatter.Format(joinDateTime), Helper.constants.shortTimeFormatter.Format(joinDateTime));
+            return String.Format(constants.r_loader.GetString("joinDate"), utilities.FormatDate(joinDateTime));
 
         }
 
