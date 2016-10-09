cd7df232b6e8ca8888ce499275bb9397d5568b01
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-10-09 17:25:16 +0200

    In search results organizations have a different UI

---
+++ Octahedron/Templates/UsersSearchResultsTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using Octokit;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Octahedron.Templates
+{
+    public class UsersSearchResultsTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate userTemplate { get; set; }
+        public DataTemplate organizationTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            return base.SelectTemplateCore(item);
+        }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            User user = (User)item;
+            if(user.Type == AccountType.User)
+            {
+                return userTemplate;
+            }
+            else
+            {
+                return organizationTemplate;
+            }
+        }
+    }
+}
